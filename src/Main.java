adb50fb32e7c494aa6b117151d135019cfc23545
aleksej <dr1ma@icloud.com>
2023-02-21 18:47:45 +0600

    fix review

--- src/Main.java
+++ src/Main.java
@@ -6,47 +6,49 @@ public class Main {
         System.out.println("Введите имя  Sveta , Anatoliy, Lena, Enakentiy, Anastasia, Aleksandr");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
-        Human result = findPeople(name,meTree);
-//        System.out.println(result.getName());
+        Human result = findPeople(name, meTree);
         System.out.println("Все дети " + result.getName() + " : ");
-        for(Human hum : result.getChild()){
+        for (Human hum : result.getChild()) {
             System.out.println(hum.getName());
         }
 
     }
-    private static Human initMe(){
-        Human mother = new Human("Sveta",Gender.WOMAN);
-        Human father = new Human("Anatoliy",Gender.MAN);
-        Human grandMotherM = new Human("Lena",Gender.WOMAN);
-        Human grandFatherM = new Human("Enakentiy",Gender.MAN);
-        Human grandFatherF = new Human("Anastasia",Gender.WOMAN);
-        Human grandMotherF = new Human("Aleksandr",Gender.MAN);
-        Human me = new Human("Ivan",Gender.MAN);
-        Human brother = new Human("Andew",Gender.MAN);
-        setParants(me,mother,father);
-        setParants(mother,grandMotherM,grandFatherM);
-        setParants(father,grandFatherF,grandMotherF);
-        setParants(brother,mother,father);
+
+    private static Human initMe() {
+        Human mother = new Human("Sveta", Gender.WOMAN);
+        Human father = new Human("Anatoliy", Gender.MAN);
+        Human grandMotherM = new Human("Lena", Gender.WOMAN);
+        Human grandFatherM = new Human("Enakentiy", Gender.MAN);
+        Human grandFatherF = new Human("Anastasia", Gender.WOMAN);
+        Human grandMotherF = new Human("Aleksandr", Gender.MAN);
+        Human me = new Human("Ivan", Gender.MAN);
+        Human brother = new Human("Andew", Gender.MAN);
+        setParants(me, mother, father);
+        setParants(mother, grandMotherM, grandFatherM);
+        setParants(father, grandFatherF, grandMotherF);
+        setParants(brother, mother, father);
         return me;
     }
-    private static void setParants(Human target, Human mom, Human dad){
+
+    private static void setParants(Human target, Human mom, Human dad) {
         target.setMom(mom);
         target.setDad(dad);
         mom.addChild(target);
         dad.addChild(target);
     }
-    private static Human findPeople(String name,Human target){
-        if (target.getName().equals(name)){
+
+    private static Human findPeople(String name, Human target) {
+        if (target.getName().equals(name)) {
             return target;
         }
         if (target.getMom() != null) {
-            Human resMom = findPeople(name,target.getMom());
+            Human resMom = findPeople(name, target.getMom());
             if (resMom != null) {
                 return resMom;
             }
         }
-        if (target.getDad() != null){
-            Human resDad = findPeople(name,target.getDad());
+        if (target.getDad() != null) {
+            Human resDad = findPeople(name, target.getDad());
             if (resDad != null) {
                 return resDad;
             }
